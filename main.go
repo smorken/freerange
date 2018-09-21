eadb962251e49e824538585674c817913d90d7f3
Scott Morken <scottm361@gmail.com>
2018-09-21 04:32:41 -0700

    changed code to use websocket example from here (with some modifications) https://github.com/gorilla/websocket/blob/master/examples/echo/server.go

--- main.go
+++ main.go
@@ -1,42 +1,60 @@
-// websockets.go
+
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"net/http"
-
+	"html/template"
 	"github.com/gorilla/websocket"
 )
+var addr = flag.String("addr", "localhost:8080", "http service address")
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+var upgrader = websocket.Upgrader{} // use default options
+
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func loadTemplate() (*template.Template) {
+//var homeTemplate = template.ParseFiles.Must(template.New("").Parse()
+	websockets, err := template.ParseFiles("websockets.html")
+	if err != nil {
+		log.Println("template:", err)
+		return nil
+	}
+	return websockets
+}
+
+var homeTemplate = loadTemplate()
+
+func home(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
 }
 
 func main() {
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
-
-		for {
-			// Read message from browser
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return
-			}
-
-			// Print the message to the console
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-			// Write message back to browser
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
-			}
-		}
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "websockets.html")
-	})
-
-	http.ListenAndServe(":8080", nil)
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
