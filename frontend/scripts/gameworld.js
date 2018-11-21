8a7f3f98567e4cb082672bab556f0754ba9ace9a
Scott Morken <scottm361@gmail.com>
2018-11-20 17:29:49 -0800

    working out gridlayer/gridlayertest

--- frontend/scripts/gameworld.js
+++ frontend/scripts/gameworld.js
@@ -18,7 +18,7 @@ function TileAtlas(id, image, nrows, ncols, xsize, ysize) {
             canvas_y_position,  //y - The y coordinate where to place the image on the canvas	
             canvas_x_size, //width - Optional. The width of the image to use (stretch or reduce the image)	
             canvas_y_size); //height - Optional. The height of the image to use (stretch or reduce the image)
-    }
+    };
 }
 
 //loads tile atlases - based on the json tileData arg
@@ -43,7 +43,7 @@ function loadTileAtlas(tileAtlas, callback) {
     for(n = 0 ; n < tileAtlas.length ; n++) {
         var id = tileAtlas[n]["id"];
         if(id in result){
-            throw `duplicate tile data id: ${id}`
+            throw `duplicate tile data id: ${id}`;
         }
         var image = new Image();
         result[id] = new TileAtlas(
@@ -56,32 +56,39 @@ function loadTileAtlas(tileAtlas, callback) {
         result[id].image.addEventListener("load", onload);
         result[id].image.src = tileAtlas[n]["src"];
     }  
-  }
+}
 
 
 
 function WorldGridLayer(nrows, ncols, tile_atlas_id){
 
     //fields for data in each cell
-    const TileAtlasRow_Field = 0
-    const TileAtlasColumn_Field = 0
+    const TileAtlasRow_Field = 0;
+    const TileAtlasColumn_Field = 0;
 
-    this.numRows = nrows
-    this.numCols = ncols
-    this.tile_atlas_id = tile_atlas_id
-    this.data = {}
+    this.numRows = nrows;
+    this.numCols = ncols;
+    this.tile_atlas_id = tile_atlas_id;
+    this.data = {};
 
     this.getvalue = function(row, col){
         if(row in this.data && col in this.data[row]){
-            return this.data[row][col]
+            return this.data[row][col];
         }
-        return undefined
+        return undefined;
     }
     // updates this layers data with x,y,value triples
     // ex: value = [[0,0,d(0,0)],[0,1,d(0,1), ... ]
     this.update = function(value){
         for(i = 0; i<value.length; i++){
-            this.data[value[0]][value[1]] = value[2]
+            if(value[0] in this.data){
+                this.data[value[0]][value[1]] = value[2];
+            }
+            else{
+                this.data[value[0]] = { };
+                this.data[value[0]][value[1]] = value[2];
+            }
+
         }
     }
 
@@ -91,20 +98,23 @@ function WorldGridLayer(nrows, ncols, tile_atlas_id){
     // @param tile_atlas_collection a dictionary of tile atlas objects    
     this.render = function(canvasView, world, tile_atlas_collection){
         tile_atlas = tile_atlas_collection[this.tile_atlas_id]
-        bounds = canvasView.getDrawBounds()
+        bounds = canvasView.getDrawBounds();
         for(row = bounds[0]; row<bounds[1]; row++){
             for(col = bounds[2]; col<bounds[3]; col++){
-                cell_data = this.getvalue(row,col)
 
-                tile_atlas.draw(
-                    canvasView.context,
-                    cell_data[TileAtlasRow_Field],
-                    cell_data[TileAtlasColumn_Field],
-                    row * world.grid_size_x - canvasView.offset_x,
-                    col * world.grid_size_y - canvasView.offset_y,
-                    gameworld.grid_size_x,
-                    gameworld.grid_size_y
-                );
+                cell_data = this.getvalue(row,col);
+                debugger;
+                if(cell_data){
+                    tile_atlas.draw(
+                        canvasView.context,
+                        cell_data[TileAtlasRow_Field],
+                        cell_data[TileAtlasColumn_Field],
+                        row * world.grid_size_x - canvasView.offset_x,
+                        col * world.grid_size_y - canvasView.offset_y,
+                        world.grid_size_x,
+                        world.grid_size_y
+                    );
+                }
             }
         }
     }
@@ -125,12 +135,12 @@ function TileBasedGameWorld(worldConfig){
     this.layers = []
 
     this.addLayer = function(layer){
-        this.layers.push(layer)
+        this.layers.push(layer);
     }
 
     this.render = function(canvasView, tileAtlasCollection){
         for(i =0; i<this.layers.length; i++){
-            this.layers[i].render(canvasView, this, tileAtlasCollection)
+            this.layers[i].render(canvasView, this, tileAtlasCollection);
         }
     }
     //get the origin x coordinate of the specified column
@@ -157,8 +167,8 @@ function TileCanvasView(canvas){
     }
 
     this.focusOn = function(actor){
-        offset_x = actor.x_position - this.size_x/2
-        offset_y = actor.y_position - this.size_y/2
+        offset_x = actor.x_position - this.size_x/2;
+        offset_y = actor.y_position - this.size_y/2;
     }
     
     //gets the draw bounds of this canvas view in world coordinates
@@ -168,6 +178,6 @@ function TileCanvasView(canvas){
             Math.floor((this.offset_y + size_y)/world.grid_size_y),
             Math.floor(this.offset_x/world.grid_size_x),
             Math.floor((this.offset_x + size_x)/world.grid_size_x)
-        ]
+        ];
     }
 }
