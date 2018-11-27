8c432aab39d6102ed9d6d106efb945d0c799b9f1
Scott Morken <scottm361@gmail.com>
2018-11-26 22:32:41 -0800

    add assert true method

--- frontend/test/test.js
+++ frontend/test/test.js
@@ -7,9 +7,9 @@ class Test{
         this.titleDiv.appendChild(document.createTextNode(title));
         this.titleDiv.style.margin = "5px 5px";
 
-        this.textDiv = document.createElement("div");
-        this.textDiv.style.margin = "5px 5px";
-        this.textDiv.style.border = "thin solid #000000";
+        this.textSectionDiv = document.createElement("div");
+        this.textSectionDiv.style.margin = "5px 5px";
+        this.textSectionDiv.style.border = "thin solid #000000";
 
         this.canvasDiv = document.createElement("div");
         this.canvasDiv.appendChild(this.canvas);
@@ -18,7 +18,7 @@ class Test{
 
         this.mainDiv = document.createElement("div");
         this.mainDiv.appendChild(this.titleDiv);
-        this.mainDiv.appendChild(this.textDiv);
+        this.mainDiv.appendChild(this.textSectionDiv);
         this.mainDiv.appendChild(this.canvasDiv);
         this.mainDiv.style.border = "thin solid #000000";
         
@@ -32,12 +32,30 @@ class Test{
     appendText(text){
         var t = document.createElement("div");
         t.appendChild(document.createTextNode(text));
-        this.textDiv.appendChild(t);
+        this.textSectionDiv.appendChild(t);
     }
 
     setText(index, text){
-        var t = this.textDiv.childNodes[index];
+        var t = this.textSectionDiv.childNodes[index];
         t.childNodes[0].nodeValue = text;
     }
+
+    AssertTrue(value, description){
+        var textDiv = document.createElement("div");
+
+        if(value){
+            textDiv.style.color = "green";
+            description = "PASS: "+ description;
+        }
+        else{
+            textDiv.style.color = "red";
+            description = "FAILED: "+ description;
+        }
+        var descNode = document.createTextNode(description)
+        textDiv.appendChild(descNode);
+        this.textSectionDiv.appendChild(textDiv);
+
+    }
+
 }
 
