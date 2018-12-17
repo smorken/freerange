1bdb2cafcc2eb19881b9a4de15b030c2a2bdbf48
Scott Morken <scottm361@gmail.com>
2018-12-16 21:59:43 -0800

    starting websocket attempt

--- server.go
+++ server.go
@@ -4,10 +4,37 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	//"github.com/gorilla/websocket"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func clientLoop(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, []byte{'a'})
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+		time.Sleep(1000 * time.Millisecond)
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -15,5 +42,6 @@ func main() {
 
 	fs := http.FileServer(http.Dir("frontend/"))
 	http.Handle("/", fs)
+	http.HandleFunc("/client", clientLoop)
 	log.Fatal(http.ListenAndServe(*addr, fs))
 }
