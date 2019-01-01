7e8e687d8e67be3c5b3a4f4d0465d2c47bc43f0e
Scott Morken <scottm361@gmail.com>
2019-01-01 13:37:09 -0800

    refactoring, moving some old scripts to folder gy (will delete eventually)

--- frontend/scripts/actor.js
+++
@@ -1,9 +0,0 @@
-
-function Actor(actorConfig) {
-
-    //the actor x position in world coordinates
-    this.x_position = 0;
-
-    //the actor y position in world coordinates
-    this.y_position = 0;
-}
