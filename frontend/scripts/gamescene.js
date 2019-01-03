a7d9cff9b2801a32320fd34a7727b58fc457fa96
Scott Morken <scottm361@gmail.com>
2019-01-03 08:33:01 -0800

    partial cleanup according to js standard linter

--- frontend/scripts/gamescene.js
+++ frontend/scripts/gamescene.js
@@ -1,83 +1,71 @@
 class GameScene extends Phaser.Scene {
-    constructor ()
-    {
-        super({ key: 'GameScene' });
-        this.messages = [];
+  constructor () {
+    super({ key: 'GameScene' })
+    this.messages = []
+  }
+
+  preload () {
+    ws.onmessage = this.getWS
+    ws.onerror = this.errorWS
+
+    // this.time.addEvent({
+    //    delay: 1000,
+    //   callback: this.sendWS,
+    //   callbackScope: this,
+    //   repeat: 100
+    // });
+  }
+  sendWS (msg) {
+    ws.send(msg);
+  }
+  getWS (evt) {
+    this.messages.push(evt.data)
+  }
+  errorWS (evt) {
+    // exit the game scene
+  }
+  create () {
+    //  A simple background for our game
+    bg = this.add.sprite(400, 300, 'bg')
+    bg.displayHeight = 600
+    bg.displayWidth = 800
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = this.physics.add.staticGroup()
+
+    //  Here we create the ground.
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+
+    player = this.physics.add.sprite(100, 450, 'santa')
+
+    player.setBounce(0.2)
+    player.setCollideWorldBounds(true)
+
+    //  Input Events
+    cursors = this.input.keyboard.createCursorKeys()
+
+    this.physics.add.collider(player, platforms)
+    this.timeText = this.add.text(100, 200)
+  }
+
+  update (time, delta) {
+    this.timeText.setText('Time: ' + time + '\nDelta: ' + delta)
+    if (cursors.left.isDown && player.body.touching.down) {
+      this.sendWS("left")
+      player.setVelocityX(-50)
+    }
+    else if (cursors.right.isDown && player.body.touching.down) {
+      this.sendWS("right")
+      player.setVelocityX(50)
+    }
+    else {
+      //this.sendWS("stop");
+      player.setVelocityX(0);
     }
 
-    preload(){
-        ws.onmessage = this.getWS;
-        ws.onerror = this.errorWS;
-
-       // this.time.addEvent({
-        //    delay: 1000,
-         //   callback: this.sendWS,
-         //   callbackScope: this,
-         //   repeat: 100
-        //});
+    if (cursors.up.isDown && player.body.touching.down) {
+      this.sendWS('stop')
+      player.setVelocityY(-150)
     }
-    sendWS(msg) {
-        ws.send(msg);
-    }
-    getWS(evt) {
-        this.messages.push( evt.data)
-    }
-    errorWS(evt) {
-        //exit the game scene
-    }
-    create ()
-    {    
-       
-
-        //  A simple background for our game
-        bg = this.add.sprite(400, 300, 'bg');
-        bg.displayHeight  = 600;
-        bg.displayWidth  =800;
-        //  The platforms group contains the ground and the 2 ledges we can jump on
-        platforms = this.physics.add.staticGroup();
-    
-        //  Here we create the ground.
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    
-        player = this.physics.add.sprite(100, 450, 'santa');
-    
-        player.setBounce(0.2);
-        player.setCollideWorldBounds(true);
-    
-        //  Input Events
-        cursors = this.input.keyboard.createCursorKeys();
-    
-        this.physics.add.collider(player, platforms);
-        this.timeText = this.add.text(100, 200);
-    }
-
-    update(time, delta)
-    {
-        this.timeText.setText('Time: ' + time + '\nDelta: ' + delta);
-        if (cursors.left.isDown && player.body.touching.down)
-        {
-            this.sendWS("left");
-            player.setVelocityX(-50);
-        }
-        else if (cursors.right.isDown && player.body.touching.down)
-        {
-            this.sendWS("right");
-            player.setVelocityX(50);
-        }
-        else
-        {
-            //this.sendWS("stop");
-            player.setVelocityX(0);
-        }
-    
-        if (cursors.up.isDown && player.body.touching.down)
-        {
-            this.sendWS("stop");
-            player.setVelocityY(-150);
-        }
-        
-    
-    }
-
+  }
 }
--- frontend/scripts/loadscene.js
+++ frontend/scripts/loadscene.js
@@ -1,38 +1,29 @@
 class LoadScene extends Phaser.Scene {
-    constructor ()
-    {
-        super({ key: 'LoadScene' });
-    }
+  constructor () {
+    super({ key: 'LoadScene' })
+  }
 
-    preload ()
-    {
-        this.load.image("bg", "https://twemoji.maxcdn.com/72x72/1f306.png");
-        this.load.image('santa', 'https://twemoji.maxcdn.com/36x36/2b1c.png');
-        this.load.image('ground', 'assets/platform.png');
+  preload () {
+    this.load.image("bg", "https://twemoji.maxcdn.com/72x72/1f306.png")
+    this.load.image('santa', 'https://twemoji.maxcdn.com/36x36/2b1c.png')
+    this.load.image('ground', 'assets/platform.png')
 
-        ws = new WebSocket("ws://localhost:8080/client");
+    ws = new WebSocket("ws://localhost:8080/client")
+    ws.onopen = this.openWS
+  }
 
-        ws.onopen = this.openWS;
+  create () {        
 
-    }
+  }
 
+  // update(){
+  //
+  // }
 
-
-    create()
-    {        
-
-    }
-
-    //update(){
-//
-    //}
-
-    //var isOpenWs = false;
-    openWS(evt){
-        game.scene.add('GameScene', GameScene, true, { x: 400, y: 300 });
-        game.scene.start('GameScene');
-        // isOpenWs = true;
-    }
-
-
+  // var isOpenWs = false;
+  openWS (evt) {
+    game.scene.add('GameScene', GameScene, true, { x: 400, y: 300 })
+    game.scene.start('GameScene')
+    // isOpenWs = true;
+  }
 }
