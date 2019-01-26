2cdb570a08703c26d5b5fb78349f8dfb70784c6b
Scott Morken <scottm361@gmail.com>
2019-01-26 01:09:33 -0800

    adding some functions for dynamic ui, clicking

--- freerangeserver/entity.go
+++ freerangeserver/entity.go
@@ -5,9 +5,12 @@ type Entity struct {
 	ID             int64
 	Img            string
 	Tags           []string
-	Xposition      int
-	Yposition      int
-	Rotation       int
+	Xposition      float64
+	Yposition      float64
+	Rotation       float64
+	Speed          float64
+	Jump           float64
+	Fly            bool
 	Xsize          int
 	Ysize          int
 	Static         bool
@@ -19,10 +22,49 @@ type Entity struct {
 	clickAction    func(level *Level)
 }
 
+//NewEntity creates an entity with all fields specified by the function parameters
+func NewEntity(Img string, Tags []string, Xposition float64, Yposition float64, Rotation float64,
+	Speed float64, Jump float64, Fly bool, Xsize int, Ysize int, Static bool, Clickable bool,
+	ParentEntityID int64, CameraChild bool, CameraParent bool, Zorder int) *Entity {
+	e := new(Entity)
+	e.Img = Img
+	e.Tags = Tags
+	e.Xposition = Xposition
+	e.Yposition = Yposition
+	e.Speed = Speed
+	e.Jump = Jump
+	e.Fly = Fly
+	e.Rotation = Rotation
+	e.Xsize = Xsize
+	e.Ysize = Ysize
+	e.Static = Static
+	e.Clickable = Clickable
+	e.ParentEntityID = ParentEntityID
+	e.CameraChild = CameraChild
+	e.CameraParent = CameraParent
+	e.Zorder = Zorder
+	e.ID = -1
+	return e
+}
+
 func actorClick(client int64, clicked *Entity) func(level *Level) {
 	return func(level *Level) {
 		level.DestroyUIEntities(client)
-		level.CreateUIEntity(client)
+		level.SetCameraParent(client, clicked)
+		left := NewEntity("left arrow", []string{"ui", "left"},
+			-10, 20, 0, 0, 0, false, 50, 50, true, true, clicked.ID, true, false, 0)
+		left.clickAction = arrowClick(left)
+		right := NewEntity("right arrow", []string{"ui", "right"},
+			-10, 20, 0, 0, 0, false, 50, 50, true, true, clicked.ID, true, false, 0)
+		right.clickAction = arrowClick(right)
+		level.AddUIEntity(client, left)
+		level.AddUIEntity(client, right)
+	}
+}
+
+func arrowClick(entity *Entity) func(level *Level) {
+	return func(level *Level) {
+		level.Move(entity.ParentEntityID, entity.Tags[1])
 	}
 }
 
@@ -115,7 +157,4 @@ func getObjectDefs() string {
         }
     ]`
 	return objectdefs
-}
-
-type CreateClickAction struct {
 }
--- freerangeserver/level.go
+++ freerangeserver/level.go
@@ -5,10 +5,23 @@ type Level struct {
 	entities map[int64]Entity
 }
 
+//Add entity adds an entity to the level
+func (level *Level) AddEntity(entity *Entity) {
+
+}
+
 func (level *Level) DestroyUIEntities(clientID int64) {
 
 }
 
-func (level *Level) CreateUIEntity(clientID int64, entity *Entity) {
+func (level *Level) AddUIEntity(clientID int64, entity *Entity) {
+
+}
+
+func (level *Level) SetCameraParent(clientID int64, entity *Entity) {
+
+}
+
+func (level *Level) Move(entityId int64, direction string) {
 
 }
