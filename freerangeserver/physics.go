972ff99fcb24aa02e1307fa752c631f952114db6
Scott Morken <scottm361@gmail.com>
2019-02-13 20:11:17 -0800

    linking entity and physics

--- freerangeserver/physics.go
+++ freerangeserver/physics.go
@@ -2,9 +2,39 @@ package freerangeserver
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/ByteArena/box2d"
 )
+
+const PixelsPerMetre float64 = 50.0
+
+//PixelCoordToPhysicsCoord converts the specified pixel coordinate pair to physics coordinates
+func PixelCoordToPhysicsCoord(x int32, y int32) (float64, float64) {
+	return float64(x) / PixelsPerMetre, float64(y) / PixelsPerMetre
+}
+
+//PhysicsCoordToPixelCoord converts the specified physics coordinate pair to pixel coordinates
+func PhysicsCoordToPixelCoord(x float64, y float64) (int32, int32) {
+	return int32(math.Round(x * PixelsPerMetre)), int32(math.Round(y * PixelsPerMetre))
+}
+
+func AddEntityBody(world *box2d.B2World, entity Entity) {
+	bodyDef := box2d.NewB2BodyDef()
+	px, py := PixelCoordToPhysicsCoord(entity.X, entity.Y)
+	bodyDef.Position.Set(px, py)
+	if entity.Static {
+		bodyDef.Type = box2d.B2BodyType.B2_staticBody
+	} else {
+		bodyDef.Type = box2d.B2BodyType.B2_dynamicBody
+	}
+	body := world.CreateBody(bodyDef)
+	box := box2d.NewB2PolygonShape()
+	box.SetAsBox(1.0, 1.0)
+	fixture := body.CreateFixture(box, 1.0)
+	fixture.SetFriction(0.3)
+
+}
 
 //Box2dTutorial is from http://box2d.org/manual.pdf
 func Box2dTutorial() {
