49234a7f3bd743d88d41b7baacedcb1c46dba2d6
Scott Morken <scottm361@gmail.com>
2019-02-24 19:42:12 -0800

    test context manager load level

--- freerangeserver/gamecontext_test.go
+++ freerangeserver/gamecontext_test.go
@@ -35,19 +35,41 @@ func TestNewGameContext(t *testing.T) {
 
 func TestLoadLevel(t *testing.T) {
 	levelmanager := new(MockLevelManager)
+	getLevelCallCount := 0
+	mockLevel := new(Level)
 	levelmanager.mockGetLevel = func(int64, LevelFactory, EntityFactory) ILevel {
-
+		getLevelCallCount++
+		return mockLevel
 	}
+	closeLevelCallCount := 0
 	levelmanager.mockCloseLevel = func(level ILevel) {
+		closeLevelCallCount++
 
 	}
-	levelFactory := func(id int64, data []Entity) ILevel {
 
+	levelFactory := func(id int64, data []Entity) ILevel {
+		return nil
 	}
 	entityFactory := func(data map[string]interface{}) Entity {
 		return Entity{}
 	}
+	mockLevelViewPort := new(LevelViewPort)
 	levelViewPortFactory := func(positionX int32, positionY int32, height int32, width int32) *LevelViewPort {
-		return nil
+		return mockLevelViewPort
+	}
+	g := NewGameContext(levelmanager, levelFactory, entityFactory, levelViewPortFactory)
+	g.LoadLevel(1)
+	if g.level != mockLevel {
+		t.Error("unexpected level")
+	}
+	if g.levelViewPort != mockLevelViewPort {
+		t.Error("unexepected level view port")
+	}
+	if getLevelCallCount != 1 || closeLevelCallCount != 0 {
+		t.Error("expected 1 get level call and no close level calls on first load")
+	}
+	g.LoadLevel(1)
+	if getLevelCallCount != 2 || closeLevelCallCount != 1 {
+		t.Error("expected 2 get level call and 1 close level calls after second load")
 	}
 }
